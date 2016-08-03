76a10ed5e324f2b67e4bad9911339077d1a16bb4
ElsaDowney <446189226@qq.com>
2016-08-03 15:55:45 +0800

    css and if()return

--- GoldenData/golden-data.jsx
+++ GoldenData/golden-data.jsx
@@ -27,7 +27,7 @@ var GoldenData = React.createClass({
     },
 
     changed: function () {
-        this.setState({isChange: [Change(this.state.isChange)]});
+        this.setState({isChange: !this.state.isChange});
     },
 
     aaa: function () {
@@ -37,82 +37,81 @@ var GoldenData = React.createClass({
     },
 
     render: function () {
-        return (
-            <div>
-                {this.state.isChange.map((item)=> {
-                    "use strict";
-                    if (item === false) {
-                        return <div>
-                            <form>
-                            <p>
-                                <button type="button" onClick={this.changed}>预览</button>
+
+        if (this.state.isChange !== false) {
+            return <div>
+                <p>
+                    <button className="ButtonCenter"
+                            type="button" onClick={this.changed}>预览
+                    </button>
+                </p>
+                
+                <form className="TextRight">
+
+                    <p>
+                        <input
+                            type="radio" name="1" onClick={this.inputs}/>文本框
+                    </p>
+                    <p>
+                        <input type="radio" name="1" onClick={this.inputs1}/>日期
+                    </p>
+                </form>
+
+                <button className="ButtonRight" type="button" onClick={this.aaa}>+</button>
+
+
+                <form className="outPut">
+
+                    {this.state.items.map((item, index)=> {
+                        if (item === 1) {
+                            return <p>
+                                <input type="text"/>
+                                <button type="button"
+                                        onClick={this.delText} value={index}>x
+                                </button>
                             </p>
+                        }
+                        else {
+                            return <p>
+                                <input type="date"/>
+                                <button type="button"
+                                        onClick={this.delDate} value={index}>x
+                                </button>
+                            </p>
+                        }
 
-                                <p>
-                                    <input type="radio" name="1" onClick={this.inputs}/>文本框
-                                </p>
-                                <p>
-                                    <input type="radio" name="1" onClick={this.inputs1}/>日期
-                                </p>
-
-                            <button type="button" onClick={this.aaa}>+</button>
-
-
-                                {this.state.items.map((item, index)=> {
-                                    if (item === 1) {
-                                        return <p>
-                                            <input type="text"/>
-                                            <button type="button"
-                                                    onClick={this.delText} value={index}>x
-                                            </button>
-                                        </p>
-                                    }
-                                    else {
-                                        return <p>
-                                            <input type="date"/>
-                                            <button type="button"
-                                                    onClick={this.delDate} value={index}>x
-                                            </button>
-                                        </p>
-                                    }
-
-                                })
-                                }
-                            </form>
-                        </div>
+                    })
                     }
-
-
-                    else {
-                        return <div>
-                            <button type="button" onClick={this.changed}>返回</button>
-                            <form>
-                                {this.state.items.map((item)=> {
-                                    if (item === 1) {
-                                        return <p>
-                                            <input type="text"/>
-                                        </p>
-                                    }
-                                    else {
-                                        return <p>
-                                            <input type="date"/>
-                                        </p>
-                                    }
-
-                                })
-                                }
-                            </form>
-                            <button type="button">提交</button>
-                        </div>
-                    }
-
-
-                })}
-
-
+                </form>
             </div>
-        )
+        }
+
+        else {
+            return <div>
+                <button className="ButtonCenter" type="button" onClick={this.changed}>返回</button>
+                <form>
+                    {this.state.items.map((item)=> {
+                        if (item === 1) {
+                            return <p>
+                                <input type="text"/>
+                            </p>
+                        }
+                        else {
+                            return <p>
+                                <input type="date"/>
+                            </p>
+                        }
+
+                    })
+                    }
+                </form>
+                <button className="ButtonCenter" type="button">提交</button>
+            </div>
+        }
+
+
     }
+
 });
 
 ReactDOM.render(<GoldenData />, document.getElementById('123'));
